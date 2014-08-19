369ad59d5dedc6e5f2fe429ab04938ec587dbf00
XROW System Account <xrow@test01.hande.c.de.siteprotect.eu>
2014-08-19 14:15:03 +0200

    optimized extension

---
+++ autoloads/ezpublisheventutils.php
@@ -0,0 +1,47 @@
+<?php
+
+class eZPEventUtils
+{
+    function eZPEventUtils()
+    {
+    }
+
+    function operatorList()
+    {
+        return array( 'ezpevent_show_weekdays' );
+    }
+
+    function namedParameterPerOperator()
+    {
+        return true;
+    }
+
+    function namedParameterList()
+    {
+        return array( 'ezpevent_show_weekdays' => array( 'languageCode' => array( 'type' => 'string',
+                                                                                  'required' => true,
+                                                                                  'default' => '' ) ) );
+    }
+
+    function modify( $tpl, $operatorName, $operatorParameters, $rootNamespace, $currentNamespace, &$operatorValue, $namedParameters )
+    {
+        switch ( $operatorName )
+        {
+            case 'ezpevent_show_weekdays':
+            {
+                $item = $operatorValue;
+                if( !isset( $item['starttime-hour'] ) || ( isset( $item['starttime-hour'] ) && $item['starttime-hour'] == '' ) )
+                    $item['starttime-hour'] = ' 00';
+                if( !isset( $item['endtime-hour'] ) || ( isset( $item['endtime-hour'] ) && $item['endtime-hour'] == '' ) )
+                    $item['endtime-hour'] = ' 00';
+                $startTimeString = trim( $item['startdate'] ) . ' ' . trim( $item['starttime-hour'] );
+                $tmpStarttime = eZPublishEvent::createDateTime( $startTimeString, $item, 'start', $namedParameters['languageCode'] );
+                $tmpStarttime->setTime( 00, 00 );
+                $endTimeString = trim( $item['enddate'] ) . ' ' . trim( $item['endtime-hour'] );
+                $tmpEndtime = eZPublishEvent::createDateTime( $endTimeString, $item, 'end', $namedParameters['languageCode'] );
+                $tmpEndtime->setTime( 00, 00 );
+                $operatorValue = ( $tmpEndtime->getTimestamp() - $tmpStarttime->getTimestamp() ) / 86400;
+            } break;
+        }
+    }
+}
---
+++ autoloads/eztemplateautoload.php
@@ -0,0 +1,7 @@
+<?php
+
+$eZTemplateOperatorArray = array();
+
+$eZTemplateOperatorArray[] = array( 'script' => 'extension/ezpublish-event/autoloads/ezpublisheventutils.php',
+                                    'class' => 'eZPEventUtils',
+                                    'operator_names' => array( 'ezpevent_show_weekdays' ) );
