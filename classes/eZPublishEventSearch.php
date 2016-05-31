55580df87518e41373e8d21ed0e3d19ee0122663
xinyuexrow <wang@xrow.de>
2016-05-31 19:35:31 +0200

    small modifications

--- classes/eZPublishEventSearch.php
+++ classes/eZPublishEventSearch.php
@@ -8,38 +8,41 @@ class eZPublishEventSearch
         if($ezpublishevent_ini->hasVariable( 'Settings', 'EventClassIdentifier' ))
         {
             $classidentifier = $ezpublishevent_ini->variable( 'Settings', 'EventClassIdentifier' );
-            $checkHidde=$object->mainNode()->IsInvisible;
-            if($checkHidde == 1)
+            if($object instanceof eZContentObject === true && !is_null($object))
             {
-                $client = eZPublishSolarium::createSolariumClient();
-                $update = $client->createUpdate();
-                $update->addDeleteQuery('meta_id_si:' . $object->ID);
-                $update->addCommit();
-                $deleteResult = $client->update($update);
-            }
-            if ($object->ClassIdentifier == $classidentifier && $object->Status == 1 && $checkHidde == 0)
-            {   
-                $client = eZPublishSolarium::createSolariumClient();
-                $query = $client->createSelect();
-                $update = $client->createUpdate();
-                
-                $query->setQuery('meta_id_si:' . $object->ID);
-                $resulttest = $client->select($query);
-                
-                if ($resulttest->getNumFound() != 0) {
+                $checkHidde=$object->mainNode()->IsInvisible;
+                if($checkHidde == 1)
+                {
+                    $client = eZPublishSolarium::createSolariumClient();
+                    $update = $client->createUpdate();
                     $update->addDeleteQuery('meta_id_si:' . $object->ID);
+                    $update->addCommit();
                     $deleteResult = $client->update($update);
                 }
-                
-                $ezpEvent = new eZPublishEvent();
-                $docList = $ezpEvent->createSOLRDocument($object, $update);
-                $update->addDocuments($docList);
-                if($commit)
+                if ($object->ClassIdentifier == $classidentifier && $object->Status == 1 && $checkHidde == 0)
                 {
-                   $update->addCommit();
+                    $client = eZPublishSolarium::createSolariumClient();
+                    $query = $client->createSelect();
+                    $update = $client->createUpdate();
+                    
+                    $query->setQuery('meta_id_si:' . $object->ID);
+                    $resulttest = $client->select($query);
+                    
+                    if ($resulttest->getNumFound() != 0) {
+                        $update->addDeleteQuery('meta_id_si:' . $object->ID);
+                        $deleteResult = $client->update($update);
+                    }
+                    
+                    $ezpEvent = new eZPublishEvent();
+                    $docList = $ezpEvent->createSOLRDocument($object, $update);
+                    $update->addDocuments($docList);
+                    if($commit)
+                    {
+                       $update->addCommit();
+                    }
+                    $result = $client->update($update);
+                    unset($docList);
                 }
-                $result = $client->update($update);
-                unset($docList);
             }
         }
     }
